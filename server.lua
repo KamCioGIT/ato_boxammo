babc93f33f2dd56f21ca03df5eee730355d8fbb5
atoshit-dev <149441640+atoshit-dev@users.noreply.github.com>
2023-12-17 22:55:36 +0100

    Update server.lua
    - Add notify system

--- server.lua
+++ server.lua
@@ -1,7 +1,14 @@
 for i = 1, #Shared.BoxAmmo do
     ESX.RegisterUsableItem(Shared.BoxAmmo[i].itemBox, function(source)
+
         local playerProps = ESX.GetPlayerFromId(source)
+
         playerProps.removeInventoryItem(Shared.BoxAmmo[i].itemBox, 1)
         playerProps.addInventoryItem(Shared.BoxAmmo[i].itemAmmo, Shared.BoxAmmo[i].receiveNumber)
+
+        if Shared.Notify then
+            TriggerClientEvent('esx:showNotification', source, 'Vous venez d\'utiliser une boîte de munitions', 'info', 3000)
+        end
+
     end)
 end
